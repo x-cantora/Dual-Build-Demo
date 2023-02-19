d69d00a25d38724d5786358cb15521880e723f25
Alec Cantor <aleccantor2@gmail.com>
2023-02-19 10:41:10 -0500

    Add no-op spi renderer

---
+++ src/Renderer/SpiRenderer.cpp
@@ -0,0 +1,6 @@
+#include "SpiRenderer.hpp"
+
+void SpiRenderer::render()
+{
+    // Todo: Implement once hardware is chosen
+}
---
+++ src/Renderer/SpiRenderer.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "Renderer.hpp"
+
+class SpiRenderer : public Renderer
+{
+public:
+    void render() override;
+};
