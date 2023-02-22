f755ffd97e1d4ad5d91111e3f1cb983d077d6b4b
Alec Cantor <alec.cantor@msasafety.com>
2023-02-22 13:01:11 -0800

    Link arduino core for arduino build

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
-#include "gears.h"
 #include "lvgl.h"
+#ifdef PC_BUILD
 #include "PcRenderer.hpp"
-#include "Triangle.hpp"
-#include "zbuffer.h"
+#else
+#include "TftRenderer.hpp"
+#endif
 
 #include <signal.h>
 
@@ -12,15 +13,20 @@ int main()
     signal(SIGINT, [](int){exit(0);});
 #endif
 
+#ifdef PC_BUILD
     PcRenderer renderer;
+#else
+    TftRenderer renderer;
+#endif
+
     auto* sdlBuffer = renderer.display()->driver->draw_buf->buf2;
 
     lv_obj_t* canvas = lv_canvas_create(lv_scr_act());
     lv_canvas_set_buffer(canvas, sdlBuffer, WIDTH, HEIGHT, LV_IMG_CF_TRUE_COLOR);
-    gears_init(WIDTH, HEIGHT, ZB_MODE_RGBA, sdlBuffer);
+    lv_canvas_fill_bg(canvas, lv_color_make(0xFF, 0x00, 0x00), LV_OPA_50);
 
-    while (1) {
-        gears_update();
+    while (1)
+    {
         renderer.render();
     }
 
