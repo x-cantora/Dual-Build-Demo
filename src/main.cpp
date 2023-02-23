c4257121f79037e3ce577aa145ad40918f6d7d30
Alec Cantor <aleccantor2@gmail.com>
2023-02-22 21:53:18 -0500

    Correctly ifdef main

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,27 @@
 #include "lvgl.h"
 #ifdef PC_BUILD
 #include "PcRenderer.hpp"
+#include <signal.h>
 #else
+#include <Arduino.h>
 #include "TftRenderer.hpp"
 #endif
-
-#include <signal.h>
 
 int main()
 {
 #ifdef PC_BUILD
     signal(SIGINT, [](int){exit(0);});
-#endif
-
-#ifdef PC_BUILD
     PcRenderer renderer;
 #else
+    init();
     TftRenderer renderer;
 #endif
 
-    auto* sdlBuffer = renderer.display()->driver->draw_buf->buf2;
+    auto* graphicsBuffer = renderer.buffer();
 
     lv_obj_t* canvas = lv_canvas_create(lv_scr_act());
-    lv_canvas_set_buffer(canvas, sdlBuffer, WIDTH, HEIGHT, LV_IMG_CF_TRUE_COLOR);
-    lv_canvas_fill_bg(canvas, lv_color_make(0xFF, 0x00, 0x00), LV_OPA_50);
+    lv_canvas_set_buffer(canvas, graphicsBuffer, WIDTH, HEIGHT, LV_IMG_CF_TRUE_COLOR);
+    lv_canvas_fill_bg(canvas, lv_color_make(0x00, 0x00, 0xFF), LV_OPA_50);
 
     while (1)
     {
